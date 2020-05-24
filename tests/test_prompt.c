df072bb904614e45f6c6175b97ad47d84c7bd1f9
Julien Pause <julien.pause@epitech.eu>
2020-05-24 15:48:03 +0400

    Fix unit tests for prompt since it changed prototype

--- tests/test_prompt.c
+++ tests/test_prompt.c
@@ -13,35 +13,48 @@
 Test(prompt, ls)
 {
     char *command_line = NULL;
+    shell_t *shell = NULL;
     char command[] = "ls";
     FILE *f_stdin = cr_get_redirected_stdin();
 
     fprintf(f_stdin, command);
     fclose(f_stdin);
-    command_line = prompt();
+    shell = shell_create(NULL);
+    command_line = prompt(&(shell->history));
     cr_assert_str_eq(command_line, command);
+    cr_assert_str_eq(shell->history->line, command);
+    cr_assert_null(shell->history->prev);
+    cr_assert_null(shell->history->next);
 }
 
 Test(prompt, ls_with_args)
 {
     char *command_line = NULL;
     char command[] = "ls -l -Rt";
+    shell_t *shell = NULL;
     FILE *f_stdin = cr_get_redirected_stdin();
 
     fprintf(f_stdin, command);
     fclose(f_stdin);
-    command_line = prompt();
+    shell = shell_create(NULL);
+    command_line = prompt(&(shell->history));
     cr_assert_str_eq(command_line, command);
+    cr_assert_str_eq(shell->history->line, command);
+    cr_assert_null(shell->history->prev);
+    cr_assert_null(shell->history->next);
 }
 
 Test(prompt, empty)
 {
     char *command_line = NULL;
     char command[] = "";
+    shell_t *shell = NULL;
     FILE *f_stdin = cr_get_redirected_stdin();
 
     fprintf(f_stdin, command);
     fclose(f_stdin);
-    command_line = prompt();
+    shell = shell_create(NULL);
+    command_line = prompt(&(shell->history));
     cr_assert_null(command_line);
+    cr_assert_null(shell->history);
 }
